0d3d3fb62b0a9d38e44961adf02a44e49bc8f0be
hewan <hewan.g167@gmail.com>
2025-07-25 11:23:45 -0600

    Add files via upload

--- Frontend_Sources/src/components/Header.js
+++ Frontend_Sources/src/components/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Header({ title, names, intro }) {
   return (
     <header className="Header">
-      <h1>{title}</h1>
-      <p>by: {names.join(', ')}</p>
-      <p>{intro}</p>
+       <h1 className="title">{title}</h1>
+      <p className="names">by: {Array.isArray(names) ? names.join(', ') : names}</p>
+      <p className="intro">{intro}</p>
     </header>
   );
 }
